e37e531dc5b09fe0609a0dac5959bf76872b67bf
“leonshimizu” <lmshimizu@gmail.com>
2021-10-28 15:48:28 -0700

    fixed seeds file to add all breweries into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,22 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 link = HTTP.get("https://api.openbrewerydb.org/breweries")
-breweries_link = link.parse(:json)
+link = link.parse(:json)
 
-# i = 0
-# 21.times do
-#   brewery = Brewery.new(
-#     name: breweries_link[i]["name"], 
-#     city: breweries_link[i]["city"], state: breweries_link[i]["state"], country: breweries_link[i]["country"])
-#   brewery.save
-#   i += 1
-# end
-
-i = 1
-5.times do
-  brewery = Brewery.new(
-        name: breweries_link[i]["name"], 
-        city: breweries_link[i]["city"], state: breweries_link[i]["state"], country: breweries_link[i]["country"])
-  brewery.save
+i = 0
+20.times do
+  brewery = Brewery.create(
+    name: link[i]["name"], 
+    city: link[i]["city"], 
+    state: link[i]["state"], 
+    country: link[i]["country"])
   i += 1
 end
