098b062490906e289b9385a8fb73b51894671605
Leon <lmshimizu@gmail.com>
2021-10-29 10:24:13 -0700

    started adding csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,55 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-link = HTTP.get("https://api.openbrewerydb.org/breweries")
-link = link.parse(:json)
+# link = HTTP.get("https://api.openbrewerydb.org/breweries")
+# link = link.parse(:json)
 
-i = 0
-20.times do
-  brewery = Brewery.create(
-    name: link[i]["name"], 
-    city: link[i]["city"], 
-    state: link[i]["state"], 
-    country: link[i]["country"])
-  i += 1
+# i = 0
+# 20.times do
+#   brewery = Brewery.create(
+#     name: link[i]["name"], 
+#     city: link[i]["city"], 
+#     state: link[i]["state"], 
+#     country: link[i]["country"])
+#   i += 1
+# end
+
+### Step 2
+# require 'csv'
+
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'breweries_us.csv'))
+# puts csv_text
+
+### Step 3
+# require 'csv'
+
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'breweries_us.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# puts csv
+
+### Step 4 
+# require 'csv'
+
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'breweries_us.csv'))
+# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+# csv.each do |row|
+#   puts row.to_hash
+# end
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'breweries_us.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Transaction.new
+  t.name = row['brewery_name']
+  t.type = row['type']
+  t.address = row['address']
+  t.website = row['website']
+  t.state = row['state']
+  t.state_breweries = row['state_breweries']
+  t.save
+  puts "#{t.name} saved"
 end
+
+puts "There are now #{Transaction.count} rows in the transactions table"
